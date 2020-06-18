bd56ef6cec9af90f83547e172c45cce30138a670
Beedeeboom <stephanie.duffieux@gmail.com>
2020-06-18 11:53:20 +1000

    first commit

---
+++ file1.js
@@ -0,0 +1,15 @@
+export const number = 1234
+
+export function add(x, y) {
+    return x + y
+}
+
+export function subtract(x, y) {
+    return x - y
+}
+
+//export {number, add, subtract}
+//export default {number, add, substract} => is the most common way of exporting 
+//with default export in the import file add the code at the top of the page like this:
+//import Steph from ".file1.js"
+//note: Steph can be named anything
---
+++ main.js
@@ -0,0 +1,14 @@
+import { add, subtract, number } from "./file1.js"
+var global = "this is a global var";
+
+console.log(add(5, 4))
+console.log(subtract(5,4))
+console.log(number)
+
+//We can also import this way by giving a name to the entire object being imported:
+import * as Steph from "./file1.js"
+var global = "this is a global var";
+
+console.log(Steph.add(5, 4))
+console.log(Steph.subtract(5,4))
+console.log(Steph.number)
